b241ab6f7913b184edc1ad5c523525455b869e1a
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-08-30 12:58:40 +0200

    init : init the composition API repos

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import {ref} from 'vue'
+
+  const message = ref("Hello world")
+</script>
+
+<template>
+  <div>
+    <h1> {{ message }}</h1>
+    <input type="text" v-model="message">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
