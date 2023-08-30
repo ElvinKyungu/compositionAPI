98b3f3a1691754c5124380a4d5981fcbce2dde92
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-08-30 13:32:16 +0200

    feat : adding the style for my card content

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <script setup lang="ts">
   import {ref} from 'vue'
 
-  const message = ref("Hello world")
+  const message = ref("Hello world");
+  
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <div class="flex justify-center flex-col h-screen items-center">
     <h1> {{ message }}</h1>
-    <form>
+    <form class="w-1/3 min-h-[10rem] bg-slate-300 py-4 px-8 bg mt-3">
       <div>
         <div>
-          <input type="text">
+          <input 
+            type="text" 
+            class="
+              w-full bg-slate-100 border 
+              border-black rounded-sm py-1 px-4 
+            "
+          >
         </div>
-        <div class="">
+        <div class="my-4">
           <label for="level">
             High task
             <input type="checkbox" id="level" >
           </label>
         </div>
+        <input 
+          type="submit" 
+          value="Ajouter" 
+          class="bg-indigo-500 text-white px-4 py-2 rounded-sm font-semibold"
+        >
       </div>
     </form>
+    <div class="w-1/3 min-h-[10rem] bg-slate-300 py-4 px-8 bg mt-3">
+      <ul class="space-y-4">
+        <li class="p-3 bg-white text-black rounded-md flex justify-between items-center">
+          <span class="font-semibold">Hello</span>
+          <div class="flex gap-2 text-white">
+            <button class="bg-red-500 p-2 ">Supprimer</button>
+            <button class="p-2 bg-indigo-500">Editer</button>
+          </div>
+        </li>
+        <li class="p-3 bg-white text-black rounded-md flex justify-between items-center">
+          <span class="font-semibold">Hello</span>
+          <div class="flex gap-2 text-white">
+            <button class="bg-red-500 p-2 ">Supprimer</button>
+            <button class="p-2 bg-indigo-500">Editer</button>
+          </div>
+        </li>
+        <li class="p-3 bg-white text-black rounded-md flex justify-between items-center">
+          <span class="font-semibold">Hello</span>
+          <div class="flex gap-2 text-white">
+            <button class="bg-red-500 p-2 ">Supprimer</button>
+            <button class="p-2 bg-indigo-500">Editer</button>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
